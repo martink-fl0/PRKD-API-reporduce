2352bbca99e895e75e1327b24bb11af8ed3de450
martinkaldramov <martinkaldramov1@gmail.com>
2023-10-24 19:58:14 +0300

    Time changes

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def hello_world():
     print("Logging message to the console.")
     current_time = time.strftime("%H:%M:%S")
-    print(f"[{current_time}] This message will be logged every 1 second.")
+    print(f"[{current_time}] is the time now.")
     return jsonify({"message": "Hello, World!"}), 200
 
 if __name__ == '__main__':

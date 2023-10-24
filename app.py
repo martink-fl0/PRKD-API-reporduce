9ba55df4ca497c9df49a2099b2e8c687da4939b8
martinkaldramov <martinkaldramov1@gmail.com>
2023-10-24 19:30:33 +0300

    Changes

--- app.py
+++ app.py
@@ -1,31 +1,11 @@
 from flask import Flask, jsonify, request, Response
-import pandas as pd
-import sys
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from bs4 import BeautifulSoup
-from webdriver_manager.chrome import ChromeDriverManager
-
 import time
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return jsonify({"message": "Hello, World!"}), 200
+    return jsonify({"message": "Logging message when accessing root /"}), 200
 
 if __name__ == '__main__':
-    print("App starting!!!")
-    print("App starting!!!")
-    print("App starting!!!")
-    print("App starting!!!")
-
-    while True:
-        current_time = time.strftime("%H:%M:%S")
-        print(f"[{current_time}] This message will be logged every 1 second.")
-        time.sleep(1)
     app.run()

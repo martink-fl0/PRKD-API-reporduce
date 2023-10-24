c8ac178871d5aed63ccfb11e9f85ebaecdaac481
martinkaldramov <martinkaldramov1@gmail.com>
2023-10-24 19:32:02 +0300

    Changes 2

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return jsonify({"message": "Logging message when accessing root /"}), 200
+    print("Logging message to the console.")
+    current_time = time.strftime("%H:%M:%S")
+        print(f"[{current_time}] This message will be logged every 1 second.")
+    return jsonify({"message": "Hello, World!"}), 200
 
 if __name__ == '__main__':
     app.run()
